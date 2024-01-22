4c80dda73be8597e0ff7d064b6d8de3ed9873091
Gabriel Larson <gabrieljosephlarson@gmail.com>
2024-01-22 13:35:09 -0700

    init commit

---
+++ Class1.cs
@@ -0,0 +1,5 @@
+﻿using ConsoleApp2;
+
+John j = new John();
+j.setName("Jimmy");
+Console.WriteLine(j.getName());
--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    internal class John
+    {
+        private static string name = "Gabe"; // when specified as 'static', one instance is created in memory and
+                                         // not created when instance of class is created. Static methods 
+                                         // can't modify non static objects
+
+        public void setName(string n)
+        {
+            name = n;
+        }
+
+        public string getName() // getter
+        {
+            return name;
+        }
+
+        private string test { get; set; } // creates a getter and setter
+    }
+}
